8d6d558e9bfa89e4ccf9f5c05b8165ee994da2f5
MvdBent <mvdbent@gmail.com>
2021-03-08 18:14:44 +0100

    added 2.7.2 Time Machine Volumes Are Encrypted (Automated)

--- 4_Security_Report.sh
+++ 4_Security_Report.sh
@@ -1280,8 +1280,31 @@ printReport
 CISLevel="1"
 audit="2.7.2 Time Machine Volumes Are Encrypted (Automated)"
 orgScore="OrgScore2_7_2"
-# $ tmDestination=$(tmutil destinationinfo)
-# $ sudo diskutil info ${tmDestination} | grep -c "Encrypted Encrypted: Yes"
+emptyVariables
+# Verify organizational score
+runAudit
+# If organizational score is 1 or true, check status of client
+if [[ "${auditResult}" == "1" ]]; then
+	method="Manual"
+	remediate="Manual > Set encryption through Disk Utility or diskutil in terminal"
+	
+	tmDestination=$(tmutil destinationinfo | grep -i NAME | awk '{print $2}')
+	tmDrives=$(tmutil destinationinfo | grep -c "NAME")
+	tmVolumeEncrypted=$(diskutil info ${tmDestination} 2>&1 | grep -c "Encrypted: Yes")
+	if [[ "${tmDrives}" -gt "0" && "${tmVolumeEncrypted}" -gt "0" ]]; then
+		result="Passed"
+		comment="Time Machine Volumes: Encrypted"
+	else 
+		if [[ "${tmDrives}" == "0" ]]; then
+			result="Passed"
+			comment="No Time Machine Volumes available"
+		else
+			result="Failed"
+			comment="Time Machine Volumes: Unencrypted"
+		fi
+	fi
+fi
+printReport
 
 CISLevel="1"
 audit="2.8 Disable Wake for network access (Automated)"
